150c48ff8323834ce4b0d888351d78618f1e69c5
Pegaso <vratpolito@gmail.com>
2019-08-24 21:22:37 +0800

    Sketch multi line, remove line, remove all lines, algorithm in VR

--- scripts/space_view3d_virtual_reality/hmd/openvr.py
+++ scripts/space_view3d_virtual_reality/hmd/openvr.py
@@ -30,11 +30,197 @@ from ..lib import (
         )
 
 import datetime
+from scipy.optimize import linear_sum_assignment
+import numpy as np
 
 currObject = ""
 currBone = ""
 currObject_l = ""
 currBone_l = ""
+
+
+# Algorithm from "New Algorithms for 2D and 3D Point Matching: Pose Estimation and Correpondance" used also in
+#   1) A New Point Matching Algorithm for Non-Rigid Registration
+#   2) Enhancing Character Posing by a Sketch-Based Interaction
+class softAss_detAnnealing_3(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    # Compute 3D distance
+    def compute_dist(self, p1, p2):
+        return math.sqrt(((p1[0] - p2[0]) ** 2) + ((p1[1] - p2[1]) ** 2) + ((p1[2] - p2[2]) ** 2))
+
+    # Move the first bone at the beggining og the stroke
+    def set_init_cond(self):
+        bones = bpy.data.objects['Armature'].pose.bones  # <H
+        stroke = bpy.data.curves['Stroke'].splines[0]  # <H
+
+        # TODO: Find root bones
+        for b in bones:
+            if b.parent == None:
+                print(bpy.data.objects['Armature'].matrix_world * b.head)
+
+                pmatrix = b.bone.matrix_local
+                omatrix = bpy.data.objects['Armature'].matrix_world
+
+
+                target_loc = bpy.data.objects['StrokeObj'].matrix_world * stroke.bezier_points[0].co
+                b.location = omatrix.inverted() * pmatrix.inverted() * target_loc
+
+                bpy.context.scene.update()
+                print(bpy.data.objects['Armature'].matrix_world * b.head)
+
+    def run(self):
+        bones = bpy.data.objects['Armature'].pose.bones  # <H
+        # stroke = bpy.data.curves['Stroke'].splines[0] # <H
+
+        dict = {}
+        stroke_points = []
+        num_points = 0
+        for k in range(0, len(bpy.data.curves['Stroke'].splines)):
+            stroke = bpy.data.curves['Stroke'].splines[k]
+            for j in range(0, len(stroke.bezier_points)):
+                p = stroke.bezier_points[j]
+                # World position of the point
+                point = bpy.data.objects['StrokeObj'].matrix_world * p.co
+                # Add into the dictionoary for correspondance
+                dict[num_points] = [k, j]  # [spline_index, point_index]
+                # Add in the point list
+                stroke_points.append(point)
+                num_points += 1
+
+        # [DEBUG]:
+        # print(dict)
+        # for i in range(0, num_points):
+        #    print(dict[i])
+        #    print(dict[i][0], dict[i][1])
+
+        # INIT PARAMETERS
+        # values of the paper
+        # beta_f = 0.2
+        # beta_r = 1.075
+        # beta = 0.00091
+        # alpha = 0.03
+        # I0 = 4
+        # I1 = 30
+
+        # custom value
+        beta_f = 0.2
+        beta_r = 1.9
+        beta = 0.00091
+        alpha = 0.03
+        I0 = 4
+        I1 = 30
+
+        iteration = 0
+
+        # set starting condition
+        self.set_init_cond()
+
+        while beta <= beta_f:
+            print('iteration:', iteration)
+
+            Qjk = []
+
+            # Compute Qjk
+            for k in range(0, num_points):
+                p = bpy.data.curves['Stroke'].splines[dict[k][0]].bezier_points[dict[k][1]]
+
+                # World position of the point
+                point = bpy.data.objects['StrokeObj'].matrix_world * p.co
+
+                costs_row = []
+                for i in range(0, len(bones)):
+                    b = bones[i]
+                    # World position of the bone
+                    tail = bpy.data.objects['Armature'].matrix_world * b.tail  # <H
+                    head = bpy.data.objects['Armature'].matrix_world * b.head  # <H
+                    # center = bpy.data.objects['Armature'].matrix_world * b.center # <H
+
+                    dist = self.compute_dist(tail, point)
+                    # length_diff = abs(b.length - compute_dist(point,head))
+                    # costs_row.append(dist + length_diff)
+                    costs_row.append(-(dist - alpha))
+
+                Qjk.append(costs_row)
+
+            m0 = np.asarray(Qjk)
+
+            # Deterministic annealing
+            for i in range(0, num_points):
+                for j in range(0, len(bones)):
+                    m0[i, j] = math.exp(beta * m0[i, j])
+
+                    # TODO: Add outlier row and cols
+
+            # Sinkhorn's method DO until m converges
+            m1 = np.ones((num_points, len(bones)))
+
+            for i in range(0, I1):  # TODO: set coverage threshold
+                for i in range(0, num_points):
+                    for j in range(0, len(bones)):
+                        m1[i, j] = m0[i, j] / np.sum(m0[i])
+
+                for i in range(0, num_points):
+                    for j in range(0, len(bones)):
+                        m0[i, j] = m1[i, j] / np.sum(m1[:, j])
+
+                        # [DEBUG]: Rows - Cols sum up
+            # print("rows - cols sum up")
+            # for i in range (0,num_points):
+            #    print (np.sum(m0[i]))
+
+            # for j in range (0, len(bones)):
+            #    print(np.sum(m0[:,j]))
+
+            # Softassign - ENERGY FORMULATION E3D
+            E3D = []
+            # Compute E3D
+            for k in range(0, num_points):
+                p = bpy.data.curves['Stroke'].splines[dict[k][0]].bezier_points[dict[k][1]]
+
+                # World position of the point
+                point = bpy.data.objects['StrokeObj'].matrix_world * p.co
+
+                costs_row = []
+                for i in range(0, len(bones)):
+                    b = bones[i]
+                    # World position of the bone
+                    tail = bpy.data.objects['Armature'].matrix_world * b.tail  # <H
+                    head = bpy.data.objects['Armature'].matrix_world * b.head  # <H
+                    # center = bpy.data.objects['Armature'].matrix_world * b.center # <H
+
+                    dist = self.compute_dist(tail, point)
+                    # length_diff = abs(b.length - compute_dist(point,head))
+                    # costs_row.append(dist + length_diff)
+                    costs_row.append(dist - alpha * m0[k, i])
+
+                E3D.append(costs_row)
+
+            cost = np.transpose(m0) * np.transpose(E3D)
+            row_ind, col_ind = linear_sum_assignment(cost)
+            print(col_ind)
+            print(cost[row_ind, col_ind].sum())
+
+            # Update pose parameters
+            for i in range(0, len(bones)):
+                b = bones[i]
+                # Set target positions
+                #
+                bpy.data.objects[b.name].location = copy.deepcopy(bpy.data.objects['StrokeObj'].matrix_world *
+                                                                  bpy.data.curves['Stroke'].splines[
+                                                                      dict[col_ind[i]][0]].bezier_points[
+                                                                      dict[col_ind[i]][1]].co)
+
+                # Articulate armature
+                constr = b.constraints['Damped Track']
+                constr.target = bpy.data.objects[b.name]
+
+            iteration += 1
+            beta = beta * beta_r
+
+
+
 
 class State(Enum):
     IDLE = 1
@@ -50,6 +236,7 @@ class State(Enum):
     SCALING = 11
     CHANGE_AXES = 12
     DRAWING = 13
+    TRACKPAD_BUTTON_DOWN = 14
 
 class StateLeft(Enum):
     IDLE = 1
@@ -68,23 +255,27 @@ class OpenVR(HMD_Base):
     curr_axes_l = 0
     state = State.IDLE
     state_l = StateLeft.IDLE
+
     diff_rot = Quaternion()
     diff_loc = bpy.data.objects['Controller.R'].location
     initial_loc = Vector((0,0,0))
     initial_rot = Quaternion()
+
     diff_rot_l = Quaternion()
     diff_loc_l = bpy.data.objects['Controller.L'].location
     initial_loc_l = Vector((0, 0, 0))
     initial_rot_l = Quaternion()
+
     diff_distance = 0
     initial_scale = 0
     trans_matrix = bpy.data.objects['Camera'].matrix_world * bpy.data.objects['Origin'].matrix_world
     diff_trans_matrix = bpy.data.objects['Camera'].matrix_world * bpy.data.objects['Origin'].matrix_world
+
     objToControll = ""
     boneToControll = ""
     objToControll_l = ""
     boneToControll_l = ""
-    zoom  = 1
+    zoom = 1
     rotFlag = True
     axes = ['LOC/ROT_XYZ','LOC_XYZ','LOC_X','LOC_Y','LOC_Z','ROT_XYZ','ROT_X','ROT_Y','ROT_Z']
 
@@ -92,7 +283,6 @@ class OpenVR(HMD_Base):
                'Controller.R', 'Controller.L',
                'Text.R', 'Text.L']
 
-    points = []
 
     def __init__(self, context, error_callback):
         super(OpenVR, self).__init__('OpenVR', True, context, error_callback)
@@ -129,7 +319,9 @@ class OpenVR(HMD_Base):
         """
 
         vrSys = openvr.init(openvr.VRApplication_Scene)
-        self.ctrl_index_r, self.ctrl_index_l, self.tracker_index, self.hmd_index  = self.findControllers(vrSys)
+        self.ctrl_index_r, self.ctrl_index_l, self.tracker_index, self.hmd_index = self.findControllers(vrSys)
+        if bpy.data.objects.get('StrokeObj') is None:
+            self.create_curve()
         bpy.data.window_managers['WinMan'].virtual_reality.lock_camera = True
 
         try:
@@ -434,7 +626,7 @@ class OpenVR(HMD_Base):
                     bpy.data.objects[obj].rotation_mode = 'QUATERNION'
 
     ## Create a curve from a set of points
-    def create_curve(self, point):
+    def create_curve(self):
         name = "Stroke"
         curvedata = bpy.data.curves.new(name=name, type='CURVE')
         curvedata.dimensions = '3D'
@@ -443,19 +635,31 @@ class OpenVR(HMD_Base):
 
         ob = bpy.data.objects.new(name + "Obj", curvedata)
         bpy.context.scene.objects.link(ob)
+        ob.show_x_ray = True
 
+    def add_spline(self, point):
+        curvedata = bpy.data.curves['Stroke']
         polyline = curvedata.splines.new('BEZIER')
         polyline.resolution_u = 1
         polyline.bezier_points[0].co = point
 
     ## Add new point to the curve
     def update_curve(self, point):
-        polyline = bpy.data.curves['Stroke'].splines[0]
+        polyline = bpy.data.curves['Stroke'].splines[-1]
         polyline.bezier_points.add(1)
         polyline.bezier_points[-1].co = point
         polyline.bezier_points[-1].handle_left = point
         polyline.bezier_points[-1].handle_right = point
         print (datetime.datetime.now())
+
+    def remove_spline(self):
+        polyline = bpy.data.curves['Stroke'].splines[-1]
+        bpy.data.curves['Stroke'].splines.remove(polyline)
+
+
+
+
+
 
 
 
@@ -498,38 +702,37 @@ class OpenVR(HMD_Base):
                     self.changeSelection(self.objToControll, self.boneToControll, False)
                     self.state = State.DECISIONAL
 
-                #INTERACTION_LOCAL - VR_BLENDER
-                if ctrl_state.ulButtonPressed == 8589934592 and self.objToControll != "":
-                    print("IDLE -> INTERACTION LOCAL")
-                    self.state = State.INTERACTION_LOCAL
-                    self.curr_axes_r = 0
+                # # INTERACTION_LOCAL - VR_BLENDER
+                # if ctrl_state.ulButtonPressed == 8589934592 and self.objToControll != "":
+                #     print("IDLE -> INTERACTION LOCAL")
+                #     self.state = State.INTERACTION_LOCAL
+                #     self.curr_axes_r = 0
+                #
+                #     if self.boneToControll != "":
+                #         self.diff_rot = ctrl.rotation_quaternion.inverted() * bpy.data.objects[self.objToControll].pose.bones[self.boneToControll].matrix.to_quaternion()
+                #         self.diff_loc = bpy.data.objects[self.objToControll].pose.bones[self.boneToControll].matrix.to_translation() - ctrl.location
+                #         self.initial_loc = copy.deepcopy(bpy.data.objects[self.objToControll].pose.bones[self.boneToControll].location)
+                #         bpy.data.objects[self.objToControll].pose.bones[self.boneToControll].rotation_mode = 'XYZ'
+                #         self.initial_rot = copy.deepcopy(bpy.data.objects[self.objToControll].pose.bones[self.boneToControll].rotation_euler)
+                #         bpy.data.objects[self.objToControll].pose.bones[self.boneToControll].rotation_mode = 'QUATERNION'
+                #
+                #     else:
+                #         self.diff_rot = ctrl.rotation_quaternion.inverted() * bpy.data.objects[self.objToControll].rotation_quaternion
+                #         self.diff_loc = bpy.data.objects[self.objToControll].location - ctrl.location
+                #         self.initial_loc = copy.deepcopy(bpy.data.objects[self.objToControll].location)
+                #         bpy.data.objects[self.objToControll].rotation_mode = 'XYZ'
+                #         self.initial_rot = copy.deepcopy(bpy.data.objects[self.objToControll].rotation_euler)
+                #         bpy.data.objects[self.objToControll].rotation_mode = 'QUATERNION'
 
-                    if self.boneToControll != "":
-                        self.diff_rot = ctrl.rotation_quaternion.inverted() * bpy.data.objects[self.objToControll].pose.bones[self.boneToControll].matrix.to_quaternion()
-                        self.diff_loc = bpy.data.objects[self.objToControll].pose.bones[self.boneToControll].matrix.to_translation() - ctrl.location
-                        self.initial_loc = copy.deepcopy(bpy.data.objects[self.objToControll].pose.bones[self.boneToControll].location)
-                        bpy.data.objects[self.objToControll].pose.bones[self.boneToControll].rotation_mode = 'XYZ'
-                        self.initial_rot = copy.deepcopy(bpy.data.objects[self.objToControll].pose.bones[self.boneToControll].rotation_euler)
-                        bpy.data.objects[self.objToControll].pose.bones[self.boneToControll].rotation_mode = 'QUATERNION'
+                # DRAWING SKETCHES - POSING_SKETCHES
+                if ctrl_state.ulButtonPressed == 8589934592:
+                    print("IDLE -> DRAWING")
+                    self.add_spline(bpy.data.objects['Controller.R'].location)
+                    self.state = State.DRAWING
 
-                    else:
-                        self.diff_rot = ctrl.rotation_quaternion.inverted() * bpy.data.objects[self.objToControll].rotation_quaternion
-                        self.diff_loc = bpy.data.objects[self.objToControll].location - ctrl.location
-                        self.initial_loc = copy.deepcopy(bpy.data.objects[self.objToControll].location)
-                        bpy.data.objects[self.objToControll].rotation_mode = 'XYZ'
-                        self.initial_rot = copy.deepcopy(bpy.data.objects[self.objToControll].rotation_euler)
-                        bpy.data.objects[self.objToControll].rotation_mode = 'QUATERNION'
-
-                # # DRAWING SKETCHES - POSING_SKETCHES
-                # if ctrl_state.ulButtonPressed == 8589934592:
-                #     print("IDLE -> DRAWING")
-                #     if bpy.data.objects.get('StrokeObj') is None:
-                #         self.create_curve(bpy.data.objects['Controller.R'].location)
-                #     self.state = State.DRAWING
-
+                #TRACKPAD.R
                 if ctrl_state.ulButtonPressed == 4294967296:
-                    x,y = ctrl_state.rAxis[0].x, ctrl_state.rAxis[0].y
-                    print (x,y)
+                    self.state = State.TRACKPAD_BUTTON_DOWN
 
                 # NAVIGATION
                 if ctrl_state.ulButtonPressed == 2:
@@ -537,11 +740,36 @@ class OpenVR(HMD_Base):
                     self.state = State.NAVIGATION_ENTER
 
             elif self.state == State.DRAWING:
-                #self.points.append(copy.deepcopy(bpy.data.objects['Controller.R'].location))
                 self.update_curve(bpy.data.objects['Controller.R'].location)
 
                 if (ctrl_state.ulButtonPressed != 8589934592):
                     self.state = State.IDLE
+
+            elif self.state == State.TRACKPAD_BUTTON_DOWN:
+
+                if ctrl_state.ulButtonPressed != 4294967296:
+                    x, y = ctrl_state.rAxis[0].x, ctrl_state.rAxis[0].y
+                    # Apply rotation for X setup otherwiise + setup
+                    x1, y1 = x * 0.707 - y * -0.707, x * -0.707 + y * 0.707
+                    x, y = x1, y1
+
+                    if x > 0 and y > 0:
+                        print('UP')
+                        print('LAUNCH ALGORITHM')
+                        softAss_detAnnealing_3().start()
+
+                    if x > 0 and y < 0:
+                        print ('RIGHT')
+                    if x < 0 and y > 0:
+                        for i in range (0, len(bpy.data.curves['Stroke'].splines)):
+                            self.remove_spline()
+                        print ('LEFT')
+                    if x < 0 and y < 0:
+                        print ('DOWN')
+                        self.remove_spline()
+
+                    self.state = State.IDLE
+
 
             elif self.state == State.DECISIONAL:
                 print("Decisional")
@@ -859,9 +1087,6 @@ class OpenVR(HMD_Base):
                     self.changeSelection(self.objToControll_l, self.boneToControll_l, True)
                     self.state_l = StateLeft.IDLE
                     print ("DECISIONAL -> IDLE")
-
-
-
 
             super(OpenVR, self).loop(context)
 
